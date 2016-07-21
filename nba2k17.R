a3635f6ce8837058ca864c3572409349fe9424c8
Pete Deevakul <editor@mozzarella.website>
2016-07-21 16:36:01 -0400

    updated model

--- nba2k17.R
+++ nba2k17.R
@@ -5,44 +5,49 @@
 
 # load data -------------------------------------------------------------------
 
-nba <- read.csv("nba2k17.csv")
+rm(list = ls())
+nba <- read.csv("nba2k17.csv", header = T)
 str(nba)
 summary(nba)
 
 # linear model 01 - wins ~ playoff --------------------------------------------
 
 table(nba$W, nba$Playoffs)
-winsPO <- as.data.frame(table(nba$W, nba$Playoffs))
+
+# look into why duplicate values show up here
+winsPO <- as.matrix(table(nba$W, nba$Playoffs))
 colnames(winsPO) <- c("NumWins", "PO", "Frequency")
 winsPO <- winsPO[order(winsPO$Frequency, decreasing = T), ]
 rownames(winsPO) <- NULL
 
 winsPO <- winsPO[!(winsPO$PO == 0 & winsPO$Frequency == 0), ]
 
+# linear regression model: wins ~ playoff berth
 winsPO.model <- lm(W ~ Playoffs, data = nba)
 summary(winsPO.model)
 # Coefficients:
-#               Estimate Std. Error t value Pr(>|t|)    
-#   (Intercept)  30.2143     0.9454   31.96   <2e-16 ***
-#   Playoffs     18.0714     1.2946   13.96   <2e-16 ***
+#               Estimate Std. Error   t value   Pr(>|t|)    
+#   (Intercept)  30.3348     0.5391     56.27   <2e-16  ***
+#   Playoffs     18.9152     0.7346     25.75   <2e-16  ***
 
-# Multiple R-squared:  0.4837
+# Multiple R-squared:  0.5686,	Adjusted R-squared:  0.5677
 
-# not that great.
+# 57% accuracy according to multiple R^2: OK. 
 
+# Correlation Matrix ----------------------------------------------------------
+
+# plot correlation of all variables, see where things lie.
 library(corrplot)
 nba.cor <- nba
 nba.cor$Team <- NULL
 nba.cor <- cor(nba.cor, use = "everything")
-# nba.cor[,colnames(nba.cor)] = apply(nba.cor[,colnames(nba.cor)], 2, 
-#                                    function(x) as.numeric(as.character(x)))
 
 par(family = "Arial Rounded MT Bold")
-corrplot(nba.cor, method = "ellipse", tl.srt = 45, tl.cex = 1, mar = c(2, 2, 2, 2),
-         title = "Correlation Matrix of Traditional NBA Statistics")
+corrplot(nba.cor, method = "ellipse", tl.srt = 45, tl.cex = 0.85, mar = c(2, 2, 2, 2),
+         title = "Correlation Matrix of Traditional NBA Statistics: 2000-2016")
 
 corrplot(nba.cor, method = "shade", tl.srt = 45, tl.cex = 1, mar = c(2, 2, 2, 2),
-         title = "Correlation Matrix of Traditional NBA Statistics", 
+         title = "Correlation Matrix of Traditional NBA Statistics: 2000-2016", 
          addCoef.col = "black", number.cex = 0.65)
 
       
