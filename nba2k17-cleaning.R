bbff4740d73494b7fab6e69609a18aca80d8e745
Pete Deevakul <editor@mozzarella.website>
2016-07-21 16:35:44 -0400

    file paths, header cleanse, comments

--- nba2k17-cleaning.R
+++ nba2k17-cleaning.R
@@ -5,16 +5,22 @@
 
 # load data -------------------------------------------------------------------
 
-# test
+# test - no need to run
 nba2k11 <- read.csv("data/2k11.csv")
+# add year
 nba2k11$Year <- 2011
+# add playoff binary (1 = made it; 0 = early summer)
 nba2k11$Playoffs <- ifelse(grepl("\\*", nba2k11$Team), 1, 0)
 
-# bind all the 2000s data into one frame
-dir <- "~/Documents/Foundations-Linear/NBA/data"
-dir2 <- "~/Documents/Foundations-Linear/NBA/data-opp"
-dir3 <- "~/Documents/Foundations-Linear/NBA/data-s"
-nbaStats <- list.files(path = dir, pattern = "csv", all.files = T, 
+# bind all the 2000s data into one frame ------------------
+
+# directory variables
+dir <- "~/GitHub/nba2k17/data"
+dir2 <- "~/GitHub/nba2k17/data-opp"
+dir3 <- "~/GitHub/nba2k17/data-s"
+
+# create file lists to call from
+nbaStats <- list.files(path = dir, pattern = "csv", all.files = T,
                        full.names = T, recursive = T)
 nbaOpp <- list.files(path = dir2, pattern = "csv", all.files = T, 
                        full.names = T, recursive = T)
@@ -22,10 +28,12 @@ standings <- list.files(path = dir3, pattern = "csv", all.files = T,
                         full.names = T, recursive = T)
 
 # set up the years variable
-years <- 2010:2016
+years <- 2000:2016
 
-# open up individual csvs, add years & playoff binary columns, bind all
-# careful with column headers from basketball-reference; they vary
+# open up individual csvs; add years & playoff binary columns; bind
+# careful with raw column headers from basketball-reference; 
+# they vary from season to season.
+
 nba <- data.frame()
 for (i in 1:length(nbaStats)) {
   temp <- read.csv(nbaStats[i])
@@ -41,25 +49,35 @@ for (i in 1:length(nbaStats)) {
   nba <- rbind(nba, temp)
 }
 
-
 # reorder columns
 nba <- nba[c(1, 2, 27, 30, 25, 26, 29, 28, 3:24)]
 nba$Rk <- NULL
 
 # separate Record into W/L columns
 library(tidyr)
+
 nba <- separate(nba, Record, into = c("W", "L"), sep = "-")
 nba$W <- as.numeric(nba$W)
 nba$L <- as.numeric(nba$L)
 
-# remove duplicates from playoff asterisks
-# charlotte, new orleans, and brooklyn account for 3 more teams
+# remove duplicates from playoff asterisk notations
 nba$Team <- gsub("\\*", "", nba$Team)
 nba$Team <- as.factor(nba$Team)
+levels(nba$Team)
+# As of 2016 there are 30 teams in the league.
+# Charlotte Bobcats/Hornets, New Orleans Hornets/Pelicans, 
+# New Orleans/Oklahoma City Hornets, Vancouver Grizzlies, Seattle Supersonics, 
+# and Brooklyn Nets account for the extra levels in the variables. 
+# Essentially, changes in team ownership/location/existence.
+
+# rename some columns for cleanliness (lowercase x, slightly easier to read)
+library(plyr)
+nba <- plyr::rename(nba, replace = c("X3P" = "x3P", "X3PA" = "x3PA", "X3P_P" = "x3P_P",
+                                     "X2P" = "x2P", "X2PA" = "x2PA", "X2P_P" = "x2P_P"))
 
 # check it out
 str(nba)
 summary(nba)
 
+# write it out
 write.table(nba, file = "nba2k17.csv", sep = ",", row.names = F)
-
